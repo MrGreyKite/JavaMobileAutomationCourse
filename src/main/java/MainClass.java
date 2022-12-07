40a1a97fe0b210bc664eef61048b6cf5877523bf
MrGreyKite <nolde3@yandex.ru>
2022-12-08 02:04:44 +0300

    задание 1

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,6 @@
 public class MainClass {
 
-    public static int getLocalNumber(){
+    static int getLocalNumber(){
         return 14;
     }
 
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -4,9 +4,15 @@ import org.junit.jupiter.api.Test;
 
 public class MainClassTest {
 
-    @DisplayName("Тест на одно число")
+    @DisplayName("Тест на одно число - равенство (позитивный тест)")
     @Test
     void testGetLocalNumber(){
         Assertions.assertEquals(14, MainClass.getLocalNumber());
+    }
+
+    @DisplayName("Тест на одно число - равенство (негативный тест)")
+    @Test
+    void testGetLocalNumberWrong() {
+        Assertions.assertEquals(15, MainClass.getLocalNumber());
     }
 }

091b434c23ec591da50eae9c26b85927cd017262
MrGreyKite <nolde3@yandex.ru>
2023-01-05 23:12:05 +0300

    with error

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,8 +1,10 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.PerformsTouchActions;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,6 +12,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.net.URL;
 import java.time.Duration;
 import java.util.List;
+
+import static io.appium.java_client.touch.WaitOptions.waitOptions;
+import static io.appium.java_client.touch.offset.PointOption.point;
 
 public class AppTest {
 
@@ -22,16 +27,18 @@ public class AppTest {
         capabilities.setCapability("deviceName", "PixelOreo8");
         capabilities.setCapability("platformVersion", "8.0");
         capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("deviceOrientation", "LANDSCAPE");
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
         capabilities.setCapability("app", "C:\\Users\\Nerve\\IdeaProjects\\JavaMobileAutomationCourse\\src\\test\\resources\\apks\\org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL ("http://localhost:4723/wd/hub"), capabilities);
-
         WebElement skipButton = driver.findElement(By.id("org.wikipedia:id/fragment_onboarding_skip_button"));
         skipButton.click();
 
+
     }
+
 
 
     @AfterEach
@@ -75,6 +82,87 @@ public class AppTest {
 
     }
 
+    @Test
+    void swipeArticle() {
+        searchSomethingOnInput("Appium");
+        waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Appium']"),
+                "Not found needed article", 10);
+        waitForElementPresent(By.xpath("//*[@resource-id='pcs-edit-section-title-description']/preceding-sibling::*[@class='android.widget.TextView']"),
+                "Not found title on page");
+        swipeUp(5);
+    }
+    @Test
+    void testSwipeArticleToTheEnd() {
+        searchSomethingOnInput("Java");
+        waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Java (programming language)']"),
+                "Not found needed article", 10);
+        waitForElementPresent(By.xpath("//*[@resource-id='pcs-edit-section-title-description']/preceding-sibling::*[@class='android.widget.TextView']"),
+                "Not found title on page");
+        swipeUntilElementIsFound(By.xpath("//android.view.View[@content-desc=\"View article in browser\"]/android.widget.TextView")
+                , 50);
+    }
+
+    @Test
+    void testSaveFirstArticle() {
+        searchSomethingOnInput("Appium");
+        String articleTitle = "Appium";
+        waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + articleTitle + "']"),
+                "Not found needed article", 10);
+        waitForElementAndClick(By.id("org.wikipedia:id/page_save"), "Not found Save button", 5);
+        waitForElementAndClick(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]"),
+                "Not found back arrow in article", 5);
+        waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/search_toolbar']/android.widget.ImageButton"),
+                "Not found back arrow in search results", 5);
+        waitForElementAndClick(By.id("org.wikipedia:id/nav_tab_reading_lists"), "Not found Saved tab", 5);
+        waitForElementAndClick(By.xpath("//*[@text='Default list for your saved articles']"),
+                "Not found link on list", 5);
+        waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + articleTitle + "']"),
+                "Not found saved article", 5);
+    }
+
+    @Test
+    void testSaveArticleAndDelete() {
+        searchSomethingOnInput("Appium");
+        String articleTitle = "Appium";
+        waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + articleTitle + "']"),
+                "Not found needed article", 10);
+        waitForElementAndClick(By.id("org.wikipedia:id/page_save"), "Not found Save button", 5);
+        waitForElementAndClick(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]"),
+                "Not found back arrow in article", 5);
+        waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/search_toolbar']/android.widget.ImageButton"),
+                "Not found back arrow in search results", 5);
+        waitForElementAndClick(By.id("org.wikipedia:id/nav_tab_reading_lists"), "Not found Saved tab", 5);
+        waitForElementAndClick(By.xpath("//*[@text='Default list for your saved articles']"),
+                "Not found link on list", 5);
+        swipeToTheLeft(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + articleTitle + "']/.."),
+                "Not found element to swipe");
+        waitForElementNotPresent(By.xpath("[@text='\" + articleTitle + \"']"), "Cannot delete article", 5);
+    }
+
+    @Test
+    void testEmptySearch() {
+        String randomSearchString = RandomStringUtils.randomAlphabetic(10);
+        searchSomethingOnInput(randomSearchString);
+        String emptySearchResultsLabel = "//*[@text='No results']";
+        String resultsLocator = "org.wikipedia:id/page_list_item_title";
+        waitForElementPresent(By.xpath(emptySearchResultsLabel),
+                "Not empty results by request " + randomSearchString, 10);
+        assertElementNotPresent(By.id("org.wikipedia:id/page_list_item_title"));
+    }
+
+    @Test
+    void testChangeScreenOrientationOnResultsPage() {
+        searchSomethingOnInput("Java");
+        List<WebElement> results = waitForElementPresent(By.id("org.wikipedia:id/search_results_list"), "Not present").
+                findElements(By.id("org.wikipedia:id/page_list_item_title"));
+        String firstTitleBeforeRotation = waitForElementAndGetAttribute(By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']/android.view.ViewGroup[1]/*[@resource-id='org.wikipedia:id/page_list_item_title']"),
+                "text", "!!!", 10);
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+        String firstTitleAfterRotation = waitForElementAndGetAttribute(By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']/android.view.ViewGroup[1]/*[@resource-id='org.wikipedia:id/page_list_item_title']"),
+                "text", "Not found", 10);
+        Assertions.assertEquals(firstTitleBeforeRotation, firstTitleAfterRotation, "First title changed since rotation");
+    }
+
     private WebElement waitForElementPresent(By by, String errorMessage, long timeout) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
         wait.withMessage(errorMessage + "\n");
@@ -107,6 +195,46 @@ public class AppTest {
         WebElement e = waitForElementPresent(by, errorMessage, timeout);
         e.clear();
         return e;
+    }
+
+    private String waitForElementAndGetAttribute(By by, String attribute, String errorMessage, long timeout) {
+        WebElement e = waitForElementPresent(by, errorMessage, timeout);
+        return e.getAttribute(attribute);
+    }
+
+    private void swipeUp(int timeForSwipe) {
+        TouchAction action = new TouchAction((PerformsTouchActions) driver);
+        Dimension size = driver.manage().window().getSize();
+        int x = size.width / 2;
+        int startY = (int) (size.height * 0.9);
+        int endY = (int) (size.height * 0.2);
+        action.press(point(x, startY)).waitAction(waitOptions(Duration.ofSeconds(timeForSwipe))).
+                moveTo(point(x, endY)).release().perform();
+
+    }
+
+    private void swipeToTheLeft(By locator, String errorMessage) {
+        WebElement el = waitForElementPresent(locator, errorMessage, 10);
+        int leftX = el.getLocation().getX();
+        int rightX = leftX + el.getSize().width;
+        int upperY = el.getLocation().getY();
+        int lowerY = upperY + el.getSize().height;
+        int middleY = (upperY + lowerY) / 2;
+
+        TouchAction action = new TouchAction((PerformsTouchActions) driver);
+        action.press(point(leftX,middleY)).waitAction(waitOptions(Duration.ofSeconds(5))).moveTo(point(rightX, middleY)).release().perform();
+    }
+
+    private void swipeUntilElementIsFound(By by, int maxSwipes) {
+        int alreadySwiped = 0;
+        while(getAmountOfElements(by) == 0) {
+            if (alreadySwiped > maxSwipes) {
+                waitForElementPresent(by, "Cannot find element " + by.toString() + " while swiping");
+                return;
+            }
+            swipeUp(1);
+            ++alreadySwiped;
+        }
     }
 
     private int getAmountOfElements(By locator){
